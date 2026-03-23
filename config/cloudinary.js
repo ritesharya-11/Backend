bcf5820634af8e2af95297de3e905a427ed21d63
Ritesh Kumar <ritesharya9955@gmail.com>
2026-03-24 00:30:27 +0530

    some change

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -3,9 +3,9 @@ import {v2 as cloudnary} from "cloudinary"
 const connectCloudinary = async () => {
    
     cloudnary.config({
-        cloud_name: process.env.CLOUDNARY_NAME,
-        api_key:process.env.CLOUDNARY_API_KEY,
-        api_secret:process.env.CLOUDNARY_SECRET_KEY
+        cloud_name: process.env.CLOUDINARY_NAME,
+        api_key:process.env.CLOUDINARY_API_KEY,
+        api_secret:process.env.CLOUDINARY_SECRET_KEY
     })
 
     
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import 'dotenv/config'
 import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
 import userRouter from './routes/userRoutes.js'
+import productRouter from './routes/productRoute.js'
 
 
 
@@ -20,6 +21,7 @@ app.use(cors())
 //api endpoints
 
 app.use('/api/user', userRouter)
+app.use('/api/product',productRouter)
 
 app.get ('/',(req,res)=>{
     res.send('API Working')
